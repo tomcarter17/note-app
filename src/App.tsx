2ff4f002ff21a5acfc22251abbcd076f3928e61c
Tom Carter <tomcarter107@gmail.com>
2025-03-22 16:14:02 +0000

    feature: Create and display unsavable notes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,23 @@ import { v4 as uuidv4 } from "uuid";
 import styles from "./App.module.css";
 import { useSessionNotes } from "api/hooks/useSessionNotes";
 import Note from "Note";
+import { useCreateNote } from "api/hooks/useCreateNote";
 
 const SESSION_ID = uuidv4();
 
 export default function App() {
   const { data: notes = [], isLoading } = useSessionNotes(SESSION_ID);
+  const { mutate: createNote } = useCreateNote(SESSION_ID);
 
   return (
     <div className={styles.app}>
       <header className={styles.header}>
         <h1>Note app</h1>
       </header>
+
+      <aside className={styles.actionBar}>
+        <button onClick={() => createNote()}>Add note</button>
+      </aside>
 
       <main className={styles.content}>
         {isLoading && (
@@ -31,11 +37,7 @@ export default function App() {
           <div className={styles.noteGrid}>
             {notes.map((note) => (
               <div key={note.id} className={styles.note}>
-                <Note
-                  id={note.id}
-                  initialBody={note.body}
-                  onDelete={() => {}}
-                />
+                <Note id={note.id} initialBody={note.body} />
               </div>
             ))}
           </div>
